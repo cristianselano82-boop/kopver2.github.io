9ecf16ca430ab587c2ff62cb48cdb138b0c5b82a
cristianselano82-boop <cristianselano82@gmail.com>
2026-08-06 03:09:50 +0700

    comm

---
+++ script.js
@@ -0,0 +1,11 @@
+const loginbtn = document.getElementById('login-btn');
+
+loginbtn.addEventListener('click', function() {
+    alert('Login berhasil diklik!');
+});
+
+const herobtn = document.getElementById('hero-btn');
+
+herobtn.addEventListener('click', function() {
+    alert('selengkapnya berhasil diklik');
+});
